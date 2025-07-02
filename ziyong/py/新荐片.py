10edbea6ceb8257632690a8e3a8f7b78ed761178
woshishiq1 <90903172+woshishiq1@users.noreply.github.com>
2025-07-02 19:44:55 +0800

    Update 新荐片.py

--- ziyong/py/新荐片.py
+++ ziyong/py/新荐片.py
@@ -1,147 +1,138 @@
-# -*- coding: utf-8 -*-
-# by @嗷呜
-import concurrent.futures
-import json
-import sys
-sys.path.append('..')
-from base.spider import Spider
-
-
-class Spider(Spider):
-
-    def init(self, extend=""):
-        self.ihost=self.imgsite()
-        pass
-
-    def getName(self):
-        pass
-
-    def isVideoFormat(self, url):
-        pass
-
-    def manualVideoCheck(self):
-        pass
-
-    def destroy(self):
-        pass
-
-    host='https://api.ubj83.com'
-
-    headers={
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2012K10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36;webank/h5face;webank/1.0;netType:NETWORK_WIFI;appVersion:416;packageName:com.jp3.xg3',
-        'Accept': 'application/json, text/plain, */*',
-        'x-requested-with': 'com.jp3.xg3',
-        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    }
-
-    def imgsite(self):
-        data=self.fetch(f"{self.host}/api/appAuthConfig",headers=self.headers).json()
-        host=data['data']['imgDomain']
-        return host if host.startswith('http') else f"https://{host}"
-
-    def getfts(self,id):
-        data=self.fetch(f"{self.host}/api/crumb/filterOptions",params={'fcate_pid':id},headers=self.headers).json()
-        fts=[{
-            'key': i['key'],
-            'name':i['key'],
-            'value': [{
-                'n': j['name'],
-                'v': j['id']
-            } for j in i['data']]
-        } for i in data['data']]
-        return id,fts
-
-    def build_cl(self,data,tid=''):
-        videos=[]
-        for i in data:
-            text=json.dumps(i.get('res_categories',[]))
-            videos.append({
-                'vod_id': f"{i.get('id')}@{'67' if json.dumps('短剧') in text and '67' in text else tid}",
-                'vod_name': i.get('title'),
-                'vod_pic': f"{self.ihost}{i.get('path') or i.get('cover_image') or i.get('thumbnail')}",
-                'vod_remarks': i.get('mask'),
-                'vod_year': i.get('score'),
-            })
-        return videos
-
-    def homeContent(self, filter):
-        result = {}
-        cdata=self.fetch(f"{self.host}/api/term/home_fenlei",headers=self.headers).json()
-        hdata=self.fetch(f"{self.host}/api/dyTag/hand_data",params={'category_id':cdata['data'][0]['id']},headers=self.headers).json()
-        classes = []
-        filters = {}
-        for k in cdata['data']:
-            if 'abbr' in k:
-                classes.append({
-                    'type_name': k['name'],
-                    'type_id': k['id']
-                })
-        with concurrent.futures.ThreadPoolExecutor(max_workers=len(classes)) as executor:
-            future_to_aid = {
-                executor.submit(self.getfts, aid['type_id']): aid['type_id']
-                for aid in classes
-            }
-            for future in concurrent.futures.as_completed(future_to_aid):
-                aid = future_to_aid[future]
-                try:
-                    aid_id, fts = future.result()
-                    filters[aid_id] = fts
-                except Exception as e:
-                    print(f"Error processing aid {aid}: {e}")
-        result['class'] = classes
-        result['filters'] = filters
-        result['list'] = [item for i in hdata['data'].values() for item in self.build_cl(i)]
-        return result
-
-    def homeVideoContent(self):
-        pass
-    def categoryContent(self, tid, pg, filter, extend):
-        params={**{'fcate_pid': tid, 'page': pg}, **extend}
-        path= '/api/crumb/shortList' if tid=='67' else '/api/crumb/list'
-        data=self.fetch(f"{self.host}{path}",params=params,headers=self.headers).json()
-        result = {}
-        result['list'] = self.build_cl(data['data'],tid)
-        result['page'] = pg
-        result['pagecount'] = 9999
-        result['limit'] = 90
-        result['total'] = 999999
-        return result
-
-    def detailContent(self, ids):
-        ids=ids[0].split('@')
-        path, ikey = ('/api/detail', 'vid') if ids[-1] == '67' else ('/api/video/detailv2', 'id')
-        data=self.fetch(f"{self.host}{path}",params={ikey:ids[0]},headers=self.headers).json()
-        v=data['data']
-        if ids[-1]=='67':
-            pdata=v.get('playlist',[])
-            n,p=[pdata[0].get('source_config_name')],['#'.join([f"{i.get('title')}${i['url']}" for i in pdata])]
-        else:
-            n,p=[],[]
-            for i in v.get('source_list_source',[]):
-                n.append(i.get('name'))
-                p.append('#'.join([f"{j.get('source_name') or j.get('weight')}${j['url']}" for j in i.get('source_list',[])]))
-
-        vod = {
-            'type_name': '/'.join([i.get('name') for i in v.get('types',[])]),
-            'vod_year': v.get('year'),
-            'vod_area': v.get('area'),
-            'vod_remarks': v.get('update_cycle'),
-            'vod_actor': '/'.join([i.get('name') for i in v.get('actors',[])]),
-            'vod_content': v.get('description'),
-            'vod_play_from': '$$$'.join(n),
-            'vod_play_url': '$$$'.join(p)
+var rule = {
+    title: '荐片[优]',
+    host: 'https://api.ubj83.com',
+    homeUrl: '/api/dyTag/hand_data?category_id=88',
+    url: '/api/crumb/list?page=fypage&type=0&limit=24&fyfilter',
+    class_name: '电影&电视剧&动漫&综艺',
+    class_url: '1&2&3&4',
+    detailUrl: '/api/video/detailv2?id=fyid',
+    searchUrl: '/api/v2/search/videoV2?key=**&page=fypage',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 1,
+    filter: {
+      "0":[{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+      "1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"首推","v":"5"},{"n":"动作","v":"6"},{"n":"喜剧","v":"7"},{"n":"战争","v":"8"},{"n":"恐怖","v":"9"},{"n":"剧情","v":"10"},{"n":"爱情","v":"11"},{"n":"科幻","v":"12"},{"n":"动画","v":"13"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+      "2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"首推","v":"14"},{"n":"国产","v":"15"},{"n":"港台","v":"16"},{"n":"日韩","v":"17"},{"n":"海外","v":"18"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+      "3":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"3"},{"n":"首推","v":"19"},{"n":"海外","v":"20"},{"n":"日本","v":"21"},{"n":"国产","v":"22"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+      "4":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"4"},{"n":"首推","v":"23"},{"n":"国产","v":"24"},{"n":"海外","v":"25"},{"n":"港台","v":"26"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}]
+    },
+    filter_url: 'area={{fl.area or "0"}}&sort={{fl.sort or "update"}}&year={{fl.year or "0"}}&category_id={{fl.cateId}}',
+    filter_def: {0: {cateId: '0'},1: {cateId: '1'},2: {cateId: '2'},3: {cateId: '3'},4: {cateId: '4'}},
+    headers: {
+        'User-Agent': 'jianpian-android/350',
+        'JPAUTH': 'y261ow7kF2dtzlxh1GS9EB8nbTxNmaK/QQIAjctlKiEv'
+    },
+    timeout: 5000,
+    limit: 8,
+    play_parse: true,
+    play_json: [{
+        re: '*',
+        json: {
+            parse: 0,
+            jx: 0
         }
-        return {'list':[vod]}
-
-    def searchContent(self, key, quick, pg="1"):
-        data=self.fetch(f"{self.host}/api/v2/search/videoV2",params={'key':key,'page':pg,'pageSize':20},headers=self.headers).json()
-        return {'list':self.build_cl(data['data']),'page':pg}
-
-    def playerContent(self, flag, id, vipFlags):
-        return  {'parse': 0, 'url': id, 'header': {'User-Agent':self.headers['User-Agent']}}
-
-    def localProxy(self, param):
-        pass
-
-    def liveContent(self, url):
-        pass
+    }],
+    lazy: '',
+    推荐: $js.toString(() => {
+        var d = [];
+        let html = request(input);
+        html = JSON.parse(html).data["20"];
+        html.forEach(it => {
+            let title = it.title;
+            let img = 'https://img1.vbwus.com' + it.path;
+            d.push({
+                title: title,
+                img: img,
+                desc: (it.mask || it.playlist.title) + ' ⭐' + it.score,
+                url: it.id
+            })
+        });
+        setResult(d);
+    }),
+    一级: $js.toString(() => {
+        cateObj.tid = cateObj.tid + '';
+        if (cateObj.tid.endsWith('_clicklink')) {
+            cateObj.tid = cateObj.tid.split('_')[0];
+            input = HOST + '/api/video/search?key=' + cateObj.tid + '&page=' + +MY_PAGE;
+        }
+        var d = [];
+        let html = request(input);
+        html = JSON.parse(html).data;
+        html.forEach(it => {            
+            let imgUrl = 'https://img1.vbwus.com' + (it.thumbnail || it.path);
+            d.push({
+                title: it.title,
+                img: imgUrl,
+                desc: (it.mask || (it.playlist && it.playlist.title)) + ' ⭐' + it.score,
+                url: it.id
+            })
+        });
+        setResult(d);
+    }),
+    二级: $js.toString(() => {
+        function getLink(data) {
+            if (!data) return '';            
+            return data.map(it => it.name).join('/');
+        }        
+        try {
+            let html = request(input);
+            html = JSON.parse(html);
+            let node = html.data;            
+            let cover = 'https://img1.vbwus.com' + node.thumbnail;            
+            VOD = {
+                vod_id: node.id,
+                vod_name: node.title,
+                vod_pic: cover,
+                type_name: node.types && node.types.length > 0 ? node.types[0].name : '',
+                vod_year: node.year || '',
+                vod_area: node.area || '',
+                vod_remarks: node.score || '',
+                vod_actor: getLink(node.actors),
+                vod_director: getLink(node.directors),
+                vod_content: node.description ? node.description.strip() : ''
+            };            
+            let playMap = {};            
+            if (node.ftp_list && node.ftp_list.length > 0) {
+                playMap["边下边播超清版"] = node.ftp_list.map(it => {
+                    return it.title + "$" + it.url;
+                }).join('#');
+            }
+            if (node.source_list_source) {
+                node.source_list_source.forEach(line => {
+                    if (line.source_list && line.source_list.length > 0) {
+                        playMap[line.name] = line.source_list.map(it => {
+                            return it.weight + "$" + it.url;
+                        }).join('#');
+                    }
+                });
+            }            
+            let playFrom = [];
+            let playList = [];            
+            Object.keys(playMap).forEach(key => {
+                playFrom.push(key);
+                playList.push(playMap[key]);
+            });
+            if (playFrom.length > 0) {
+                VOD.vod_play_from = playFrom.join('$$$');
+                VOD.vod_play_url = playList.join('$$$');
+            }
+        } catch (e) {
+            log("获取二级详情页发生错误:" + e.message);
+        }
+    }),
+    搜索: $js.toString(() => {
+        var d = [];
+        let html = request(input);
+        html = JSON.parse(html).data;
+        html.forEach(it => {
+            d.push({
+                title: it.title,
+                img: 'https://img1.vbwus.com' + it.thumbnail,
+                desc: it.mask + ' ⭐' + it.score,
+                url: it.id
+            })
+        });
+        setResult(d);
+    }),
+}
